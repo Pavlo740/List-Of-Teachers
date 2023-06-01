5bd783ad45d8ab61cb65a361e05072fb3b11ad1a
Pavlo740 <pavlo.dykyi@viti.edu.ua>
2023-06-01 12:59:58 +0300

    start project

---
+++ src/main/java/com/project/listofteachers/ListOfTeachersApplication.java
@@ -0,0 +1,13 @@
+package com.project.listofteachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfTeachersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfTeachersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/listofteachers/ListOfTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.listofteachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfTeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
